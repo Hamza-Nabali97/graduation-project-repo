39758157d8754417dd5e4bac5dece00fd6427272
hamza hejja <hamza.hejja@dimensions-healthcare.net>
2018-09-17 14:00:02 +0300

    Login Page Ionic Implementation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
 
   constructor(public navCtrl: NavController) {
 
   }
+
+  hideShowPassword() {
+    this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+    this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+  }
+
+  navigateToSignUpPage(){
+    window.alert('Sign Up!');
+  }
+
+
+
 
 }
